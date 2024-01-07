df4ceff3e69fb0495c5399862f4ebb932742e629
Emil Backlund <emil_backi_@hotmail.com>
2024-01-07 16:48:38 +0100

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 const Layout = ({ children, isHovered }) => {
   return (
-    <body
-      className={` h-screen font-figtree text-black overflow-hidden transiton duration-500 ${
-        isHovered ? 'bg-black/90' : 'bg-yellow'
-      }`}
-    >
-      {children}
-    </body>
+    <div className={` h-screen font-figtree text-black `}>
+      <div
+        className={`h-full overflow-hidden transiton duration-500 ${
+          isHovered ? 'bg-black/90' : 'bg-yellow'
+        }`}
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
